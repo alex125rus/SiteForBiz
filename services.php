86a89bedd2bacc7355d34f589cd1c5541621e7df
alex125rus <alex125rus2013@yandex.ru>
2022-11-08 20:04:41 +0300

    add servises in page

--- services.php
+++ services.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Главня</title>
     <link rel="stylesheet" href="css/menu.css">
 </head>
+
 <body>
     <header>
-        <h1>Сервис по выбору детективов</h1>
+        <h1 class="center">Сервис по выбору детективов</h1>
     </header>
-    <div class="top">
-        <ul class="topmenu">
+    <div class="top" class="center">
+        <ul class="topmenu center">
             <li>
                 <a href="index.php">Главная</a>
             </li>
@@ -27,41 +29,90 @@
                 <a href="application.php">Приложение</a>
             </li>
         </ul>
+        <div class="center clockdiv">
+            <a href="https://time.is/Obninsk" id="time_is_link" rel="nofollow"
+                style="font-size:64px;color:white;background:black"></a>
+            <span id="Obninsk_z71d" style="font-size:64px;color:white;background:black"></span>
+            <script src="//widget.time.is/t.js"></script>
+            <script>
+            time_is_widget.init({
+                Obninsk_z71d: {}
+            });
+            </script>
+        </div>
         <br>
-        <label>Вы находитесь на главной странице.</label>
+        <div class="center">
+            <div class="look-calendar" style="color: black;">
+                <table id="calendar">
+                    <thead>
+                        <tr>
+                            <td><b>‹</b>
+                            <td colspan="5">
+                            <td><b>›</b>
+                        <tr class="dn">
+                            <td>Пн
+                            <td>Вт
+                            <td>Ср
+                            <td>Чт
+                            <td>Пт
+                            <td>Сб
+                            <td>Вс
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+        </div>
+        <script src="js/calendar.js"></script>
         <br>
-        <label>Данный сайт реализован в рамках лабораторного практикума</label>
-        <br>
-        <label>По предмету "Web технологии".</label>
-        <br>
-        <label>Для взаимодествия с БД "Радиодетали" нажмите на кнопку "Приложение".</label>
-        <br>
-        <br>
-        <h1>Сервисы</h1>
-        <br>
+        <div class="center">
+            <label>Вы находитесь на главной странице.</label>
+            <br>
+            <label>Данный сайт реализован в рамках лабораторного практикума</label>
+            <br>
+            <label>По предмету "Web технологии".</label>
+            <br>
+            <label>Для взаимодествия с БД "Радиодетали" нажмите на кнопку "Приложение".</label>
+            <br>
+            <br>
+            <h1>Сервисы</h1>
+            <br>
+        </div>
     </div>
-    <div>
-                <!-- Yandex.Metrika informer -->
-        <a href="https://metrika.yandex.ru/stat/?id=91098061&amp;from=informer"
-        target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/91098061/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
-        style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="91098061" data-lang="ru" /></a>
+    <div class="center">
+        <!-- Yandex.Metrika informer -->
+        <a href="https://metrika.yandex.ru/stat/?id=91098061&amp;from=informer" target="_blank" rel="nofollow"><img
+                src="https://informer.yandex.ru/informer/91098061/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика"
+                title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)"
+                class="ym-advanced-informer" data-cid="91098061" data-lang="ru" /></a>
         <!-- /Yandex.Metrika informer -->
 
         <!-- Yandex.Metrika counter -->
-        <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode
+                .insertBefore(k, a)
+        })
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
         ym(91098061, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true
         });
         </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/91098061" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <noscript>
+            <div><img src="https://mc.yandex.ru/watch/91098061" style="position:absolute; left:-9999px;" alt="" /></div>
+        </noscript>
         <!-- /Yandex.Metrika counter -->
     </div>
 </body>
