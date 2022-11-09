2ddbbdda9cf6aec6d58ee9795c3047be8ac1a723
alex125rus <alex125rus2013@yandex.ru>
2022-11-09 18:42:10 +0300

    vix view

--- index.php
+++ index.php
@@ -37,42 +37,55 @@
         </ul>
         <br>
         <div class="center">
-        <label>Добро пожаловать!</label>
-        <br>
-        <label>Вы находитесь на главной странице.</label>
-        <br>
-        <label>Данный сайт реализован в рамках лабораторного практикума</label>
-        <br>
-        <label>По предмету "Web программирование".</label>
-        <br>
-        <label>Для взаимодествия с БД "Детективы" нажмите на кнопку "Приложение".</label>
+            <label>Добро пожаловать!</label>
+            <br>
+            <label>Вы находитесь на главной странице.</label>
+            <br>
+            <label>Данный сайт реализован в рамках лабораторного практикума</label>
+            <br>
+            <label>По предмету "Web программирование".</label>
+            <br>
+            <label>Для взаимодествия с БД "Детективы" нажмите на кнопку "Приложение".</label>
         </div>
         <br>
     </div>
     <br><br><br><br><br><br><br>
     <div class="center">Copyright © 2022 Дуванова Анастасия</div>
     <div class="center">
-                <!-- Yandex.Metrika informer -->
-        <a href="https://metrika.yandex.ru/stat/?id=91098061&amp;from=informer"
-        target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/91098061/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
-        style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="91098061" data-lang="ru" /></a>
+        <!-- Yandex.Metrika informer -->
+        <a href="https://metrika.yandex.ru/stat/?id=91098061&amp;from=informer" target="_blank" rel="nofollow"><img
+                src="https://informer.yandex.ru/informer/91098061/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика"
+                title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)"
+                class="ym-advanced-informer" data-cid="91098061" data-lang="ru" /></a>
         <!-- /Yandex.Metrika informer -->
 
         <!-- Yandex.Metrika counter -->
-        <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode
+                .insertBefore(k, a)
+        })
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
         ym(91098061, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true
         });
         </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/91098061" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <noscript>
+            <div><img src="https://mc.yandex.ru/watch/91098061" style="position:absolute; left:-9999px;" alt="" /></div>
+        </noscript>
         <!-- /Yandex.Metrika counter -->
     </div>
 </body>
